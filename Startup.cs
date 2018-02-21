51317faed1d2d8cf43a5004f41e2df7938239b84
digitopoi <hubo85@gmail.com>
2018-02-21 16:45:12 -0600

    Add user class'

--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TestAPI.Models;
 
 namespace TestAPI
 {
